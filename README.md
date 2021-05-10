a8aa225f7664b5b1972f6ead3d2b4ea4e99ca8e2
Scott Kao <scottxxx666@gmail.com>
2021-05-11 02:19:03 +0800

    [refactor] Rename project to web assembly

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# go-webassembly
+# webassembly
--- go/main.go
+++ go/main.go
@@ -12,23 +12,21 @@ import (
 )
 
 func main() {
-	document := js.Global().Get("document")
-	log.Println(document)
-	canvas := document.Call("getElementById", "canvas")
-	log.Println(document)
-	ctx := canvas.Call("getContext", "2d")
-	canvas.Set("width", js.ValueOf(500))
-	canvas.Set("height", js.ValueOf(500))
-
-	image := js.Global().Call("eval", "new Image()")
-	image.Set("src", "data:image/png;base64,"+loadImage("image.png"))
-
-	js.Global().Call("setInterval", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		ctx.Call("clearRect", 0, 0, 500, 500)
-		ctx.Call("drawImage", image, 0, 0)
-
-		return nil
-	}), js.ValueOf(50))
+	// document := js.Global().Get("document")
+	// canvas := document.Call("getElementById", "canvas")
+	// ctx := canvas.Call("getContext", "2d")
+	// canvas.Set("width", js.ValueOf(500))
+	// canvas.Set("height", js.ValueOf(500))
+	//
+	// image := js.Global().Call("eval", "new Image()")
+	// image.Set("src", "data:image/png;base64,"+loadImage("image.png"))
+	//
+	// js.Global().Call("setInterval", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	// 	ctx.Call("clearRect", 0, 0, 500, 500)
+	// 	ctx.Call("drawImage", image, 0, 0)
+	//
+	// 	return nil
+	// }), js.ValueOf(50))
 
 	select {}
 }
